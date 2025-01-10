9c21d5fca4df0e4be83812a0904c1763216002ad
manhtuan <nguyenmanhtuancomputer@gmail.com>
2025-01-11 02:16:04 +0700

    tuan

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -64,42 +64,15 @@ import {
 //   ],
 // })
 
-// @Module({
-//   imports: [
-//     TypeOrmModule.forRoot({
-//       type: 'postgres',
-//       host: 'pg-3867fca-nguyenmanhtuancomputer-4939.l.aivencloud.com',
-//       port: 22085,
-//       username: 'avnadmin',
-//       password: 'AVNS_uDO6u2sbSeZxpBzE0xT',
-//       database: 'defaultdb',
-//       entities: [
-//         HistoryEntity,
-//         UserEntity,
-//         LabEntity,
-//         RoleEntity,
-//         ScheduleEntity,
-//         RequestEntity,
-//         NotifyEntity,
-//       ],
-//       synchronize: false,
-//       ssl: {
-//         rejectUnauthorized: false, // Cần khi Railway yêu cầu SSL
-//       },
-//       // autoLoadEntities: true,
-//     }),
-//   ],
-// })
-
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'postgres.railway.internal',
-      port: 5432,
-      username: 'postgres',
-      password: 'ImPPkUPtHcUNuAPDfIRLxwMHKdIcPlMd',
-      database: 'railway',
+      host: 'pg-3867fca-nguyenmanhtuancomputer-4939.l.aivencloud.com',
+      port: 22085,
+      username: 'avnadmin',
+      password: 'AVNS_uDO6u2sbSeZxpBzE0xT',
+      database: 'defaultdb',
       entities: [
         HistoryEntity,
         UserEntity,
